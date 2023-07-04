211cf884fd3bf8633f578edfbebc3c0f995c1149
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-07-03 23:46:24 -0300

    Add files via upload

---
+++ java/Programação_Orientada_a_Objetos/src/entities/triangle.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class triangle {
+
+	public double a;
+	public double b;
+	public double c;
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	
+	}
+
+	//Usado em "Criando_um_método_para_obtermos_os_benefícios_de_reaproveitamento_e_delegação"
+	
+}
