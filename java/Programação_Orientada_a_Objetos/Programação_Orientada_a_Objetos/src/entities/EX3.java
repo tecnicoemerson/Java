754a3a414fe8c490da7814853339a9689720ff74
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-07-04 23:49:27 -0300

    Add files via upload

---
+++ java/Programação_Orientada_a_Objetos/Programação_Orientada_a_Objetos/src/entities/EX3.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class EX3 {
+
+	//usado em "exercicio3"
+	
+	public String name;
+	public double nota1 , nota2, nota3;
+	
+	public double soma() {
+		return nota1 + nota2 + nota3;
+	}
+	
+	
+	public double notafinal(){
+		 if(soma() < 60.0) {
+			return 60.0 - soma();
+			}
+		 else {
+			 return 0.0;
+		 }
+	}
+	
+	
+}
