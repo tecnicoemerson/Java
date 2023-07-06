1a5fa547f8af1f0077f82834f840dd1e122a9186
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-07-05 23:57:46 -0300

    Add files via upload

---
+++ java/Programação_Orientada_a_Objetos/src/entities/fixa.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class fixa{
+	
+	//utilizado em "exercicio_de_fixacao"
+	
+	public static double dolar;
+	public static double quantidade;
+	
+	public static double soma() {
+		return dolar * quantidade * (1 + 0.06);
+	}
+	
+
+}
---
+++ java/Programação_Orientada_a_Objetos/src/exercicio/exercicio_de_fixacao.java
@@ -0,0 +1,26 @@
+package exercicio;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.fixa;
+
+public class exercicio_de_fixacao {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("What is the dollar price? ");
+		 fixa.dolar = sc.nextDouble();
+		
+		System.out.println("How many dollars will be bought? ");
+		fixa.quantidade = sc.nextDouble();
+		
+		System.out.printf("Amount to be paid in reais = %.2f%n" , fixa.soma());
+		
+		sc.close();
+	}
+
+}
