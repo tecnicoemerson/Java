f2339950250267f76a9d4d0b9346e2746018c962
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-07-11 20:56:19 -0300

    Add files via upload

---
+++ java/Comportamento_de_memória_arrays_listas/src/vetor/aula88.java
@@ -0,0 +1,36 @@
+package vetor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.product;
+
+public class aula88 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		product[] vect = new product[n];
+		
+		for(int i=0;i<vect.length;i++) {
+			sc.nextLine();
+			String name = sc.nextLine();
+			double price = sc.nextDouble();
+			vect[i] = new product(name,price);
+		}
+		
+		double sum = 0;
+		for(int i=0;i<vect.length;i++) {
+			sum += vect[i].getPrice();
+		}
+		double avg = sum / vect.length;
+		System.out.printf("avarege height: %.2f%n", avg);
+		
+		sc.close();
+
+	}
+
+}
