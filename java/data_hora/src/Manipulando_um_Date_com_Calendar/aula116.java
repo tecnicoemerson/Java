1e4ec57f2421e41b9f70030df825136962884dae
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-07-23 23:52:38 -0300

    Add files via upload

---
+++ java/data_hora/src/Manipulando_um_Date_com_Calendar/aula116.java
@@ -0,0 +1,29 @@
+package Manipulando_um_Date_com_Calendar;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class aula116 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Somando uma unidade de tempo");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		System.out.println(sdf.format(d));
+	
+		Calendar cal = Calendar.getInstance(); 
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		
+		System.out.println(sdf.format(d));
+	
+		
+		
+	}
+
+}
