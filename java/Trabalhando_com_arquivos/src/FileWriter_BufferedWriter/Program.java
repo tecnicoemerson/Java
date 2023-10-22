c56e88edcc4b43bd961a4b4e81ff53a92877bf11
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-10-22 02:16:24 -0300

    Add files via upload

---
+++ java/Trabalhando_com_arquivos/src/FileWriter_BufferedWriter/Program.java
@@ -0,0 +1,27 @@
+package FileWriter_BufferedWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"bom dia", "boa tarde", "boa noite"};
+
+		String path = "/home/emerson/Documentos/out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ java/Trabalhando_com_arquivos/src/Informações_do_caminho_do_arquivo/Program.java
@@ -0,0 +1,25 @@
+package Informações_do_caminho_do_arquivo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a  folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+		
+		sc.close();
+		
+	}
+
+}
