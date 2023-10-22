47912cce8399aed811e1034712b6fce30f65e6a2
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-10-22 02:03:25 -0300

    Update and rename exercicio1.java to Treinamento1.java

--- java/Comportamento_de_memória_arrays_listas/src/exercicio_vetor/Treinamento1.java
+++ java/Comportamento_de_memória_arrays_listas/src/exercicio_vetor/Treinamento1.java
@@ -3,7 +3,7 @@ package exercicio_vetor;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class exercicio1 {
+public class Treinamento1 {
 
 	public static void main(String[] args) {
 		
