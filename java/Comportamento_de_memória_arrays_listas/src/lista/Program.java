7dbe8444a635067f76882d00567f6d7604b01859
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-10-22 01:43:25 -0300

    Update and rename aula98.java to Program.java

--- java/Comportamento_de_memória_arrays_listas/src/lista/Program.java
+++ java/Comportamento_de_memória_arrays_listas/src/lista/Program.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class aula98 {
+public class Program {
 
 	public static void main(String[] args) {
 		
