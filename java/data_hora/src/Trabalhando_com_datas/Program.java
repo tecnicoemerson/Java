1636d489b4554a83e97b19b634b5b3df22218226
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-10-22 01:41:08 -0300

    Update and rename aula115.java to Program.java

--- java/data_hora/src/Trabalhando_com_datas/Program.java
+++ java/data_hora/src/Trabalhando_com_datas/Program.java
@@ -6,7 +6,7 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class aula115 {
+public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		
