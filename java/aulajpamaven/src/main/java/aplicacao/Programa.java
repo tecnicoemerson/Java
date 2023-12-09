1af93484ddc3641ae384a00717c14fbe1f33e372
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-12-08 23:07:46 -0300

    Add files via upload

--- java/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ java/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -10,17 +10,29 @@ public class Programa {
 
 	public static void main(String[] args) {
 
+		// PARA SALVAR NA TABELA
+		Pessoa p1 = new Pessoa(null,"Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null,"Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null,"Ana Maria", "ana@gmail.com");
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Pessoa p = em.find(Pessoa.class, 2);
 		em.getTransaction().begin();
-		em.remove(p);
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
 		em.getTransaction().commit();
+		
+		//REMOVER QUALQUER UM NA TABELA
+		//Pessoa p = em.find(Pessoa.class, 2);
+		//em.getTransaction().begin();
+		//em.remove(p);
+		//em.getTransaction().commit();
     
-		System.out.println("Pronto!");
-		em.close();
-		emf.close();
+		System.out.println("Pronto !");
+		//em.close();
+		//emf.close();
 	}
 
 }
